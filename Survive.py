b4c035c5004005a82c86ca01c2a3acaf57550509
Guilherme Leite <guipleite@gmail.com>
2018-03-29 09:34:28 -0300

    Pseudo-desviador
     arquivo com o que foi feito até o agora na parte de sobrevivencia

---
+++ Survive.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+
+import numpy as np
+
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+
+
+def scaneou(dado):
+	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+	print("Leituras:")
+	Distancias = np.array(dado.ranges).round(decimals=2)
+	#print(Distancias[315])
+	for distancia in Distancias[300:]: #define visão de 300 a 360 graus
+
+		if distancia < 0.5 and distancia != 0.0: #distancia de 0,5 metro e filtra os dados infalidos
+			print(distancia)
+
+			velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 3))  #velocidade angular
+		else: 
+			velocidade = Twist(Vector3(0.2, 0.2, 0.2), Vector3(0, 0, 0)) #anda pra frente devagar
+
+		velocidade_saida.publish(velocidade)
+	#print("Intensities")
+	#print(np.array(dado.intensities).round(decimals=2))
+
+
+if __name__=="__main__":
+
+	rospy.init_node("le_scan")
+
+	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+
+
+
+	while not rospy.is_shutdown():
+		print("Oeee")
+		
+		rospy.sleep(2)
+
