cf2092d049d1ac0353808e8a1c13c42b37b2eb83
elijose55 <elijose55@hotmail.com>
2018-04-09 17:56:44 -0300

    codigo de deteccao funcionando e testado

--- ros/projeto1/scripts/deteccao.py
+++ ros/projeto1/scripts/deteccao.py
@@ -16,6 +16,10 @@ from cv_bridge import CvBridge, CvBridgeError
 bridge = CvBridge()
 
 cv_image = None
+media = []
+centro = []
+atraso = 1.5
+achou = 0
 
 check_delay = False # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados
 
@@ -26,15 +30,18 @@ FLANN_INDEX_KDITREE = 0
 flannParam = dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
 flann = cv2.FlannBasedMatcher(flannParam,{})
 
-img = cv2.imread("madfox.jpg",0)
+img1 = cv2.imread("/home/borg/catkin_ws/src/Projeto1-Robotica/ros/projeto1/scripts/madfox.jpg",0)
+time.sleep(6)
 trainKP,trainDesc = sift.detectAndCompute(img1,None)
 
 
 
 
 def detecta_imagem(frame):
+	achou = 0
 
 ######## algoritmo de deteccao da imagem (copiada do projeto madfox)
+
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray,(5,5),0)
 	bordas_color = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
@@ -46,7 +53,8 @@ def detecta_imagem(frame):
 			goodMatch.append(m)
 	MIN_MATCH_COUNT = 30
 	if len(goodMatch) > MIN_MATCH_COUNT:
-		confirmacao += 2
+		achou = 1
+
 		tp = []
 		qp = []
 		for m in goodMatch:
@@ -88,14 +96,15 @@ def detecta_imagem(frame):
 		media = (0,0)
 		print "Raposa nao encontrada- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
 
-
-	return media, centro
-	#return tamanho
+	print('funcionando')
+	centro = (frame.shape[0]//2, frame.shape[1]//2)
+	return media, centro, achou
 
 
 def roda_todo_frame(imagem):
-	print("frame")
+	print("teste")
 	global cv_image
+	global achou
 	global media
 	global centro
 
@@ -108,54 +117,44 @@ def roda_todo_frame(imagem):
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		media, centro = detecta_imagem(cv_image)
+		media, centro, achou = detecta_imagem(cv_image)
 		depois = time.clock()
 		cv2.imshow("Camera", cv_image)
 	except CvBridgeError as e:
 		print('ex', e)
 
 if __name__=="__main__":
+	print('funcionando1')
 
 	rospy.init_node("projeto")
+	print('funcionando2')
 	# Para usar a Raspberry Pi
-	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
-	
+	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
+	print('funcionando3')
 	# Para usar a webcam 
 	#recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+	print('funcionando4')
 
 	try:
 
 		while not rospy.is_shutdown():
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 
-'''
-############## testar abaixo
-			while len(media) != 0 and len(centro) != 0:
+			if len(media) != 0 and len(centro) != 0 and achou == 1:
 				dif_x = media[0]-centro[0]
 				dif_y = media[1]-centro[1]
-				while dif_x > 0: # Vira a esquerda
-					vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
-					dif_x = media[0]-centro[0]
-				while dif_x < 0: # Vira a direita
-					vel = Twist(Vector3(0,0,0), Vector3(0,0,0.2))
-			velocidade_saida.publish(vel)
-			rospy.sleep(0.01)
-'''
-
-			#################
-
-			if len(media) != 0 and len(centro) != 0:
-				dif_x = media[0]-centro[0]
-				dif_y = media[1]-centro[1]
-				if math.fabs(dif_x)<30: # Se a media estiver muito proxima do centro anda para frente
-					vel = Twist(Vector3(0.5,0,0), Vector3(0,0,0))
-				else:
-					if dif_x > 0: # Vira a direita
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
-					else: # Vira a esquerda
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.2))
+				if dif_x < 0: # Vira a esquerda
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,-5))
+					time.sleep(0.7)
+					print('esquerda')
+				if dif_x > 0: # Vira a direita
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,5))
+					time.sleep(0.7)
+			else: 
+				print('nao achou')
+				vel = Twist(Vector3(0.01,0,0), Vector3(0,0,0))
 			velocidade_saida.publish(vel)
 			rospy.sleep(0.01)
 
