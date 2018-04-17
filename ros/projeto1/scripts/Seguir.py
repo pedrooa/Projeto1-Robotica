010f4ed8ff95ccdd6695bc08933d4b1007935066
Nicolas <nicolasbs@al.insper.edu.br>
2018-04-17 18:20:30 -0300

    controleproporcional

--- ros/projeto1/scripts/Seguir.py
+++ ros/projeto1/scripts/Seguir.py
@@ -9,10 +9,14 @@ from sensor_msgs.msg import LaserScan
 def segue(velocidade_saida, media_objeto, centro_objeto):
 	vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 	if len(media_objeto) != 0 and len(centro_objeto) != 0:
-		dif_x = media_objeto[0]-centro_objeto[0]
-		dif_y = media_objeto[1]-centro_objeto[1]
+		dif_x = media_objeto[0] - centro_objeto[0]
+		dif_y = media_objeto[1] - centro_objeto[1]
 		if math.fabs(dif_x)<30: # Se a media_objeto estiver muito proxima do centro_objeto anda para frente
 			vel = Twist(Vector3(0.02,0,0), Vector3(0,0,0))
+		elif dif_x > 150:
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.6))
+		elif dif_x < -150:
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,0.6))
 		else:
 			if dif_x > 0: # Vira a direita
 				vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
